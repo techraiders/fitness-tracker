2062c76999d118a1dab971cc2f9832ce832b87d1
Navneet Prakash <hello_navneet@hotmail.com>
2018-11-11 16:19:46 +0530

    Adds sorting feature in dataTable of angular material

--- src/app/training/past-trainings/past-trainings.component.ts
+++ src/app/training/past-trainings/past-trainings.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { MatTableDataSource } from '@angular/material';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { MatTableDataSource, MatSort } from '@angular/material';
 import { Exercise } from '../exercise.model';
 import { TrainingService } from '../training.service';
 
@@ -8,14 +8,19 @@ import { TrainingService } from '../training.service';
   templateUrl: './past-trainings.component.html',
   styleUrls: ['./past-trainings.component.scss']
 })
-export class PastTrainingsComponent implements OnInit {
+export class PastTrainingsComponent implements OnInit, AfterViewInit {
   displayedColumns: Array<string> = ['date', 'name', 'duration', 'calories', 'state'];
   dataSource = new MatTableDataSource<Exercise>();
+  @ViewChild(MatSort) sort : MatSort;
 
   constructor(private trainingService : TrainingService) { }
 
   ngOnInit() {
     this.dataSource.data = this.trainingService.exercises;
+  }
+
+  ngAfterViewInit () {
+    this.dataSource.sort = this.sort;
   }
 
 }
--- src/app/training/training.service.ts
+++ src/app/training/training.service.ts
@@ -9,8 +9,8 @@ export class TrainingService {
 
   private _availableExercises : Array<Exercise> = [
     {id: 'crunches', name: 'Crunches', duration: 30, calories: 8},
-    {id: 'touch-toes', name: 'Touch Toes', duration: 180, calories: 15},
-    {id: 'side-lunges', name: 'Side Lunges', duration: 120, calories: 18},
+    {id: 'touch-toes', name: 'Touch Toes', duration: 10, calories: 15},
+    {id: 'side-lunges', name: 'Side Lunges', duration: 20, calories: 18},
     {id: 'burpees', name: 'Burpees', duration: 60, calories: 8}
   ];
   private _runningExercise : Exercise;
