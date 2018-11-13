c89f2cee8d5184515887655a2293b462ba4e1897
Navneet Prakash <hello_navneet@hotmail.com>
2018-11-13 19:41:57 +0530

    Creates sharedModule, authModule, and trainingModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,43 +1,33 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { FlexLayoutModule } from "@angular/flex-layout";
 
 import { AppRoutingModule } from "./app-routing.module";
-import { MaterialModule } from "./material.module";
 import { AngularFireModule } from "@angular/fire";
 import { AngularFirestoreModule } from "@angular/fire/firestore";
 
 import { AppComponent } from "./app.component";
-
-import { TrainingComponent } from "./training/training.component";
-import { CurrentTrainingComponent } from "./training/current-training/current-training.component";
-import { NewTrainingComponent } from "./training/new-training/new-training.component";
-import { PastTrainingsComponent } from "./training/past-trainings/past-trainings.component";
 import { WelcomeComponent } from "./welcome/welcome.component";
 import { HeaderComponent } from "./navigation/header/header.component";
 import { SidenavListComponent } from "./navigation/sidenav-list/sidenav-list.component";
 import { StopTrainingComponent } from "./training/current-training/stop-training.component";
 import { environment } from "../environments/environment";
 import { AuthModule } from "./auth/auth.module";
+import { TrainingModule } from "./training/training.module";
+import { SharedModule } from "./shared/shared.module";
 
 @NgModule({
   declarations: [
     AppComponent,
-    TrainingComponent,
-    CurrentTrainingComponent,
-    NewTrainingComponent,
-    PastTrainingsComponent,
     WelcomeComponent,
     HeaderComponent,
-    SidenavListComponent,
-    StopTrainingComponent
+    SidenavListComponent
   ],
   imports: [
     AppRoutingModule,
-    MaterialModule,
     AuthModule,
-    FlexLayoutModule,
+    TrainingModule,
+    SharedModule,
     BrowserModule,
     BrowserAnimationsModule,
     AngularFireModule.initializeApp(environment.firebase),
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,25 +1,14 @@
 import { NgModule } from "@angular/core";
 import { AngularFireAuthModule } from "@angular/fire/auth";
+import { ReactiveFormsModule } from "@angular/forms";
 
 import { SignupComponent } from "./signup/signup.component";
 import { LoginComponent } from "./login/login.component";
-import { CommonModule } from "@angular/common";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-
-import { MaterialModule } from "../material.module";
-import { FlexLayoutModule } from "@angular/flex-layout";
+import { SharedModule } from "../shared/shared.module";
 
 @NgModule({
   declarations: [SignupComponent, LoginComponent],
-
-  imports: [
-    CommonModule,
-    FormsModule,
-    ReactiveFormsModule,
-    MaterialModule,
-    FlexLayoutModule,
-    AngularFireAuthModule
-  ],
+  imports: [ReactiveFormsModule, AngularFireAuthModule, SharedModule],
   exports: []
 })
 export class AuthModule {}
