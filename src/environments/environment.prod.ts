336fc18f768373572dee3523cba6db7c239e3e8c
Navneet Prakash <hello_navneet@hotmail.com>
2018-11-28 15:39:29 +0530

    Adds firebase config to production env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyA5wO3IB3WO8cAmOfK-O1rxnxaXa8sCePA",
+    authDomain: "ng-fitness-tracker-afe0c.firebaseapp.com",
+    databaseURL: "https://ng-fitness-tracker-afe0c.firebaseio.com",
+    projectId: "ng-fitness-tracker-afe0c",
+    storageBucket: "ng-fitness-tracker-afe0c.appspot.com",
+    messagingSenderId: "89375651036"
+  }
 };
